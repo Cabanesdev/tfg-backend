71e3ddbaf1ffcf79f7fcd3f2d53c5779b899daea
Mark Cabanes <cabanesdev@gmail.com>
2022-01-09 17:25:20 +0100

    create(userModel): create a user schema

--- components/user/model.js
+++ components/user/model.js
@@ -1,7 +1,36 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema();
+const userSchema = new Schema({
+	username: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	firstName: {
+		type: String,
+		required: true,
+	},
+	surname1: {
+		type: String,
+		required: true,
+	},
+	surname2: {
+		type: String,
+		required: false,
+	},
+	birthday: {
+		type: Date,
+		required: false,
+	},
+	creationDate: {
+		type: Date,
+		required: true,
+	},
+});
 
 const userModel = mongoose.model('user', userSchema, 'user');
 
