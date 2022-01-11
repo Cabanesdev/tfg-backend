a7c9921174f433060a10cca45581bbe2147db768
Mark Coronado Cabanes <cabanesdev@gmail.com>
2022-01-11 15:04:58 +0100

    feature(userNetwork):
    add validator in create user

--- components/user/network.js
+++ components/user/network.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const response = require('../../network/responses');
 
+const {
+	registerSchema,
+} = require('../../middleware/validators/user.validator');
 const { createUser } = require('./controller');
 
 const router = express.Router();
 
 router.post('/create', (req, res) => {
+	const { error } = registerSchema.validate(req.body);
+
+	if (error)
+		return response.error(req, res, 'Error', 400, error.details[0].message);
+
 	createUser(req.body)
 		.then((data) => {
 			console.log(data);
