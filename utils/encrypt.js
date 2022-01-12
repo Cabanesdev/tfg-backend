a4f3844e2ae4f62bb5f2fe3c4a3711d53669f643
Mark Cabanes <cabanesdev@gmail.com>
2022-01-12 11:39:42 +0100

    create encrypt util

---
+++ utils/encrypt.js
@@ -0,0 +1,10 @@
+const bcrypt = require('bcrypt');
+
+const encryptPass = async (password) => {
+	const passwordEncrypted = await bcrypt.hash(password, 10);
+	return passwordEncrypted;
+};
+
+module.exports = {
+	passwordEncrypted,
+};
