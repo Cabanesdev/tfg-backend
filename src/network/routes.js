24d655ec54b995bf803c9a5a389ffbe10e643e0c
Mark Cabanes <cabanesdev@gmail.com>
2022-01-20 18:21:11 +0100

    add endpoint to routes.js

--- src/network/routes.js
+++ src/network/routes.js
@@ -1,7 +1,9 @@
 const user = require('@components/user/network');
+const post = require('@components/post/network');
 
 const routes = (server) => {
 	server.use('/user', user);
+	server.use('/post', post);
 };
 
 module.exports = routes;
