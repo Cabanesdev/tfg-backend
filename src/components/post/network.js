2a0f7cca38d89bbf7ef6a8019e581a64a072c0dc
Mark Coronado Cabanes <cabanesdev@gmail.com>
2022-03-27 00:13:45 +0100

    chore(PostNetwork): delte comments and cahnge format

--- src/components/post/network.js
+++ src/components/post/network.js
@@ -4,102 +4,99 @@ const router = express.Router();
 const { createSchema, editSchema } = require('@validators/post.validator');
 const { validateToken } = require('@src/middleware/jwt');
 const {
-	createPost,
-	getById,
-	getByUserId,
-	editPost,
-	deletePost,
+  createPost,
+  getById,
+  getByUserId,
+  editPost,
+  deletePost,
 } = require('./controller');
 
 const response = require('@responses');
 
 router.get('/:id', (req, res) => {
-	const id = req.params.id;
+  const id = req.params.id;
 
-	if (!id)
-		return response.error(
-			req,
-			res,
-			'Error',
-			400,
-			'Cannot retrieve a post without an id'
-		);
+  if (!id)
+    return response.error(
+      req,
+      res,
+      'Error',
+      400,
+      'Cannot retrieve a post without an id'
+    );
 
-	getById(id)
-		.then((data) => response.succes(req, res, 'Get Post', 200, data))
-		.catch((err) => response.error(req, res, 'Error', 400, err));
+  getById(id)
+    .then((data) => response.succes(req, res, 'Get Post', 200, data))
+    .catch((err) => response.error(req, res, 'Error', 400, err));
 });
 
-//getByUserId pagination
 router.get('/', validateToken, (req, res) => {
-	const userId = req.query.userId;
+  const userId = req.query.userId;
 
-	if (!userId)
-		return response.error(req, res, 'Error', 400, 'User Id can not be empty');
-	const page = req.query.page | 1;
+  if (!userId)
+    return response.error(req, res, 'Error', 400, 'User Id can not be empty');
+  const page = req.query.page | 1;
 
-	getByUserId(userId, page)
-		.then((data) => response.succes(req, res, 'Get Post by User', 200, data))
-		.catch((err) => response.error(req, res, 'Error', 400, err));
+  getByUserId(userId, page)
+    .then((data) => response.succes(req, res, 'Get Post by User', 200, data))
+    .catch((err) => response.error(req, res, 'Error', 400, err));
 });
 
 router.post('/', validateToken, (req, res) => {
-	const { error } = createSchema.validate(req.body);
+  const { error } = createSchema.validate(req.body);
 
-	if (error) {
-		const { getValidationErrorMessage } = require('@utils/errorUtils');
-		const errorMessage = getValidationErrorMessage(error);
-		return response.error(req, res, 'Error', 400, errorMessage);
-	}
+  if (error) {
+    const { getValidationErrorMessage } = require('@utils/errorUtils');
+    const errorMessage = getValidationErrorMessage(error);
+    return response.error(req, res, 'Error', 400, errorMessage);
+  }
 
-	createPost(req.userId, req.body)
-		.then((data) => response.succes(req, res, 'Create Post', 200, data))
-		.catch((err) => response.error(req, res, 'Error', 200, err));
+  createPost(req.userId, req.body)
+    .then((data) => response.succes(req, res, 'Create Post', 200, data))
+    .catch((err) => response.error(req, res, 'Error', 200, err));
 });
 
-//edit post
 router.put('/:id', validateToken, (req, res) => {
-	const postId = req.params.id;
+  const postId = req.params.id;
 
-	if (!postId)
-		return response.error(
-			req,
-			res,
-			'Error',
-			400,
-			'Cannot edit a post without an id'
-		);
+  if (!postId)
+    return response.error(
+      req,
+      res,
+      'Error',
+      400,
+      'Cannot edit a post without an id'
+    );
 
-	const newData = req.body;
-	const { error } = editSchema.validate(newData);
+  const newData = req.body;
+  const { error } = editSchema.validate(newData);
 
-	if (error) {
-		const { getValidationErrorMessage } = require('@utils/errorUtils');
-		const errorMessage = getValidationErrorMessage(error);
-		return response.error(req, res, 'Error', 400, errorMessage);
-	}
+  if (error) {
+    const { getValidationErrorMessage } = require('@utils/errorUtils');
+    const errorMessage = getValidationErrorMessage(error);
+    return response.error(req, res, 'Error', 400, errorMessage);
+  }
 
-	editPost(postId, req.userId, newData)
-		.then(() => response.succes(req, res, 'edit Post', 204))
-		.catch((err) => response.error(req, res, 'edit Post', 400, err));
+  editPost(postId, req.userId, newData)
+    .then(() => response.succes(req, res, 'edit Post', 204))
+    .catch((err) => response.error(req, res, 'edit Post', 400, err));
 });
 
-//Delete post
 router.delete('/:id', validateToken, (req, res) => {
-	const postId = req.params.id;
+  const postId = req.params.id;
 
-	if (!postId)
-		return response.error(
-			req,
-			res,
-			'Error',
-			400,
-			'Cannot delete a post without an id'
-		);
+  if (!postId)
+    return response.error(
+      req,
+      res,
+      'Error',
+      400,
+      'Cannot delete a post without an id'
+    );
 
-	deletePost(postId, req.userId)
-		.then(() => response.succes(req, res, 'Delete Post', 204))
-		.catch((err) => response.error(req, res, 'Delete Post', 400, err));
+  deletePost(postId, req.userId)
+    .then(() => response.succes(req, res, 'Delete Post', 204))
+    .catch((err) => response.error(req, res, 'Delete Post', 400, err));
 });
 
 module.exports = router;
