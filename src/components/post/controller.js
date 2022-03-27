e3d0156a17c4f61793b6ff7bf49a34dd66584f9e
Mark Coronado Cabanes <cabanesdev@gmail.com>
2022-03-27 23:59:22 +0200

    chore(Post):Fix import typo in post controller

--- src/components/post/controller.js
+++ src/components/post/controller.js
@@ -1,44 +1,50 @@
-const { create, getPostById, edit, deleteById } = require('./store');
+const {
+  create,
+  getPostById,
+  edit,
+  deleteById,
+  getPostsByUserId,
+} = require('./store');
 
 const getById = (id) =>
-	new Promise(async (resolve) => resolve(await getPostById(id)));
+  new Promise(async (resolve) => resolve(await getPostById(id)));
 
 const getByUserId = (userId, page) =>
-	new Promise(async (resolve) => resolve(await getPostsByUserId(userId, page)));
+  new Promise(async (resolve) => resolve(await getPostsByUserId(userId, page)));
 
 const createPost = (userId, body) => {
-	const { title, content } = body;
-	const creationDate = new Date().toLocaleDateString(); // mm/dd/yyyy
+  const { title, content } = body;
+  const creationDate = new Date().toLocaleDateString(); // mm/dd/yyyy
 
-	const newPost = {
-		title,
-		content,
-		userId,
-		creationDate,
-	};
+  const newPost = {
+    title,
+    content,
+    userId,
+    creationDate,
+  };
 
-	return new Promise(async (resolve) => {
-		const post = await create(newPost);
-		resolve(post);
-	});
+  return new Promise(async (resolve) => {
+    const post = await create(newPost);
+    resolve(post);
+  });
 };
 
 const editPost = (postId, userId, newData) => {
-	return new Promise(async (resolve, reject) => {
-		const { userId: postOwner } = await getPostById(postId);
-		if (userId !== postOwner) return reject('You are not allowed to do this');
+  return new Promise(async (resolve, reject) => {
+    const { userId: postOwner } = await getPostById(postId);
+    if (userId !== postOwner) return reject('You are not allowed to do this');
 
-		resolve(await edit(postId, newData));
-	});
+    resolve(await edit(postId, newData));
+  });
 };
 
 const deletePost = (postId, userId) => {
-	return new Promise(async (resolve, reject) => {
-		const { userId: postOwner } = await getPostById(postId);
-		if (userId !== postOwner) return reject('You are not allowed to do this');
+  return new Promise(async (resolve, reject) => {
+    const { userId: postOwner } = await getPostById(postId);
+    if (userId !== postOwner) return reject('You are not allowed to do this');
 
-		resolve(await deleteById(postId));
-	});
+    resolve(await deleteById(postId));
+  });
 };
 
 module.exports = { getById, getByUserId, createPost, editPost, deletePost };
