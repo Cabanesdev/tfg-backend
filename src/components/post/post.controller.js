e2f25f313fccfc54ed6c29caacd342d02ff6f982
Mark Coronado Cabanes <cabanesdev@gmail.com>
2022-04-30 13:25:38 +0200

    chore(): refactoring post component

--- src/components/post/post.controller.js
+++ src/components/post/post.controller.js
@@ -6,45 +6,61 @@ const {
   getPostsByUserId,
 } = require('./post.repository');
 
-const getById = (id) =>
-  new Promise(async (resolve) => resolve(await getPostById(id)));
+const getById = async (id) => {
+  try {
+    const post = await getPostById(id);
+    return post;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}
 
-const getByUserId = (userId, page) =>
-  new Promise(async (resolve) => resolve(await getPostsByUserId(userId, page)));
+const getByUserId = async (userId, page) => {
+  try {
+    const posts = await getPostsByUserId(userId, page);
+    return posts;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}
 
-const createPost = (userId, body) => {
-  const { title, content } = body;
- 
-  const newPost = {
-    title,
-    content,
-    userId,
-    creationDate: new Date(),
-    comments: 0
-  };
+const createPost = async (userId, body) => {
+  try {
+    const { title, content } = body;
+    const newPost = {
+      title,
+      content,
+      userId,
+      creationDate: new Date(),
+      comments: 0
+    };
 
-  return new Promise(async (resolve) => {
-    const post = await create(newPost);
-    resolve(post);
-  });
-};
+    await create(newPost);
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}
 
-const editPost = (postId, userId, newData) => {
-  return new Promise(async (resolve, reject) => {
+const editPost = async (postId, userId, newData) => {
+  try {
     const { userId: postOwner } = await getPostById(postId);
-    if (userId !== postOwner) return reject('You are not allowed to do this');
+    if (userId !== postOwner) throw new Error('You are not allowed to do this');
 
-    resolve(await edit(postId, newData));
-  });
-};
+    await edit(postId, newData)
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}
 
-const deletePost = (postId, userId) => {
-  return new Promise(async (resolve, reject) => {
+const deletePost = async (postId, userId) => {
+  try {
     const { userId: postOwner } = await getPostById(postId);
-    if (userId !== postOwner) return reject('You are not allowed to do this');
+    if (userId !== postOwner) throw new Error('You are not allowed to do this');
 
-    resolve(await deleteById(postId));
-  });
-};
+    await deleteById(postId);
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}
 
 module.exports = { getById, getByUserId, createPost, editPost, deletePost };
