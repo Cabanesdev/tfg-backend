c354fa68d3e26f628caebbecb1b1e124de2fb3a8
Mark Coronado Cabanes <cabanesdev@gmail.com>
2022-04-30 21:20:13 +0200

    chore(): refactoring user component

--- src/components/user/user.controller.js
+++ src/components/user/user.controller.js
@@ -7,83 +7,84 @@ const {
   getUserByUsername,
   edit,
   getUsersByUsername,
-  // deleteById,
 } = require('./user.repository');
 
 const { encryptPass, comparePass } = require('../../utils/encrypt');
 
 const createUser = async (body) => {
-  const { username, email, password, name } = body;
-  const encryptedPassword = await encryptPass(password);
+  try {
+    const { username, email, password, name } = body;
+    const encryptedPassword = await encryptPass(password);
 
-  const newUser = {
-    username,
-    email,
-    name,
-    password: encryptedPassword,
-    creationDate: new Date()
-  };
+    const newUser = {
+      username,
+      email,
+      name,
+      password: encryptedPassword,
+      creationDate: new Date()
+    };
 
-  const existsUsername = await checkUsername(username);
-  const existsEmail = await checkEmail(email);
+    const existsUsername = await checkUsername(username);
+    const existsEmail = await checkEmail(email);
 
-  return new Promise(async (resolve, reject) => {
-    if (existsUsername) return reject('Username is already being used');
-    if (existsEmail) return reject('Email is already being used');
+    if (existsUsername) throw new Error('Username is already being used');
+    if (existsEmail) throw new Error('Email is already being used');
 
     await create(newUser);
-    resolve();
-  });
+  } catch (err) {
+    throw new Error(err.message);
+  }
 };
 
 const login = async (body) => {
-  const { username, password } = body;
-  const userExists = await checkUsername(username);
+  try {
+    const { username, password } = body;
+    const userExists = await checkUsername(username);
 
-  return new Promise(async (resolve, reject) => {
     if (userExists) {
       const { password: encryptedPassword } = await getPasswordByUsername(
         username
       );
       const isPasswordCorrect = await comparePass(password, encryptedPassword);
-      if (isPasswordCorrect) resolve(await getUserByUsername(username));
+      if (isPasswordCorrect) return await getUserByUsername(username);
     }
 
-    reject('Username or password is not correct');
-  });
+    throw new Error('Username or password is not correct');
+  } catch (err) {
+    throw new Error(err.message);
+  }
 };
 
 const editUser = async (id, body) => {
-  const { username, email } = body;
-
-  return new Promise(async (resolve, reject) => {
+  try {
+    const { username } = body;
     if (username) {
       const existsUsername = await checkUsername(username);
-      if (existsUsername) return reject('Username is already being used');
-    }
-
-    if (email) {
-      const existsEmail = await checkEmail(email);
-      if (existsEmail) return reject('Email is already being used');
+      if (existsUsername) throw new Error('Username is already being used');
     }
 
     await edit(id, body);
-    resolve('User has been updated');
-  });
+  } catch (err) {
+    throw new Error(err.message);
+  }
 };
 
 const searchByUsername = async (username, page) => {
-  return new Promise(async (resolve) => {
+  try {
     const users = await getUsersByUsername(username, page);
-    resolve(users);
-  });
+    return users;
+  } catch (err) {
+    throw new Error(err.message);
+  }
 };
 
 const getUser = async (userId) => {
-  return new Promise(async (resolve) => {
+  try {
     const users = await getUserById(userId);
-    resolve(users);
-  });
+    return users;
+  } catch (err) {
+    throw new Error(err.message);
+  }
 };
 
 module.exports = {
--- src/components/user/user.network.js
+++ src/components/user/user.network.js
@@ -14,105 +14,93 @@ const {
 
 const router = express.Router();
 
-router.get('', (req, res) => {
-  const username = req.query.username;
-  let page = req.query.page || 1;
+router.get('', async (req, res) => {
+  try {
+    const username = req.query.username;
+    let page = req.query.page || 1;
 
-  if (!username)
-    return response.error(
-      req,
-      res,
-      'Error',
-      400,
-      'Username filter cant be empty string'
-    );
+    if (!username)
+      throw new Error('Username filter cant be empty string');
 
-  searchByUsername(username, page)
-    .then((data) => response.succes(req, res, 'FilterUser', 200, data))
-    .catch((err) => response.error(req, res, 'Error', 400, err));
+    const users = await searchByUsername(username, page)
+    response.succes(req, res, 'FilterUser', 200, users)
+  } catch (err) {
+    response.error(req, res, 'Error', 400, err.message);
+  }
 });
 
-router.get('/session', validateToken, (req, res) => {
-  getUser(req.userId)
-    .then((data) => response.succes(req, res, 'Get User', 200, data))
-    .catch((err) => response.error(req, res, 'Error', 400, err));
+router.get('/session', validateToken, async (req, res) => {
+  try {
+    const user = await getUser(req.userId)
+    response.succes(req, res, 'Get User', 200, user)
+
+  } catch (err) {
+    response.error(req, res, 'Error', 400, err.message);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    if (req.params.id.length !== 24)
+      throw new Error('Id Format wrong');
 
-  if (req.params.id.length !==24 )
-    return response.error(
-      req,
-      res,
-      'Error',
-      400,
-      'Id Format wrong'
-    );
-
-  getUser(req.params.id)
-    .then((data) => response.succes(req, res, 'Get User', 200, data))
-    .catch((err) => response.error(req, res, 'Error', 400, err));
-});
-
-router.post('', (req, res) => {
-  const { error } = registerSchema.validate(req.body);
-
-  if (error) {
-    const errorMessage = getValidationErrorMessage(error);
-    return response.error(req, res, 'Error', 400, errorMessage);
+    const user = await getUser(req.params.id)
+    response.succes(req, res, 'Get User', 200, user);
+  } catch (err) {
+    response.error(req, res, 'Error', 400, err.message);
   }
 
-  createUser(req.body)
-    .then(() => {
-      response.succes(req, res, 'Create User', 200, 'User Created Succesfully');
-    })
-    .catch((err) => {
-      response.error(req, res, 'Error', 400, err);
-    });
 });
 
-router.post('/login', (req, res) => {
-  const { error } = loginSchema.validate(req.body);
+router.post('', async (req, res) => {
+  try {
+    const { error } = registerSchema.validate(req.body);
 
-  if (error) {
-    const errorMessage = getValidationErrorMessage(error);
-    return response.error(req, res, 'Error', 400, errorMessage);
+    if (error) {
+      const errorMessage = getValidationErrorMessage(error);
+      return response.error(req, res, 'Error', 400, errorMessage);
+    }
+    await createUser(req.body)
+    response.succes(req, res, 'Create User', 200, 'User Created Succesfully');
+
+  } catch (err) {
+    response.error(req, res, 'Error', 400, err.message)
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const { error } = loginSchema.validate(req.body);
+
+    if (error) {
+      const errorMessage = getValidationErrorMessage(error);
+      throw new Error(errorMessage);
+    }
+
+    const user = await login(req.body)
+    const token = createToken(user);
+    res.set('bearer-token', token);
+    response.succes(req, res, 'User', 200, 'Login successfully')
+  } catch (err) {
+    response.error(req, res, 'Error', 400, err.message);
+  }
+});
+
+router.put('', validateToken, async (req, res) => {
+  try {
+    const { error } = editSchema.validate(req.body);
+
+    if (error) {
+      const errorMessage = getValidationErrorMessage(error);
+      return response.error(req, res, 'Error', 400, errorMessage);
+    }
+
+    await editUser(req.userId, req.body)
+    response.succes(req, res, 'User Edited', 204, '');
+  } catch (err) {
+    response.error(req, res, 'Error', 400, err.message);
   }
 
-  login(req.body)
-    .then((data) => {
-      const token = createToken(data);
-      res.set('bearer-token', token);
-      response.succes(req, res, 'User', 200, 'Login successfully')
-    })
-    .catch((err) => {
-      response.error(req, res, 'Error', 400, err.message);
-    });
 });
-
-router.put('', validateToken, (req, res) => {
-  const { error } = editSchema.validate(req.body);
-
-  if (error) {
-    const errorMessage = getValidationErrorMessage(error);
-    return response.error(req, res, 'Error', 400, errorMessage);
-  }
-
-  editUser(req.userId, req.body)
-    .then((data) => {
-      response.succes(req, res, 'User Edited', 204, data);
-    })
-    .catch((err) => {
-      response.error(req, res, 'Error', 400, err);
-    });
-});
-
-// router.delete('', validateToken, (req, res) => {
-//   deleteUser(req.userId)
-//     .then((data) => {
-//       response.succes(req, res, 'Delete user', 204, data);
-//     })
-//     .catch((err) => response.error(req, res, 'Error', 400, err));
-// });
 
 module.exports = router;
