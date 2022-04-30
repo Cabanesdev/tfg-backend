941a41fb1dc184fcb26f50b5cc66b7476bb268bf
Mark Coronado Cabanes <cabanesdev@gmail.com>
2022-04-30 02:03:13 +0200

    chore(): refactoring comment component

--- src/components/comment/comment.network.js
+++ src/components/comment/comment.network.js
@@ -2,40 +2,47 @@ const express = require('express');
 const { validateToken } = require('../../utils/jwt');
 const { getValidationErrorMessage } = require('../../utils/errorUtils');
 const response = require('../../network/responses');
-const { createComment, pagination } = require('./comment.controller');
+const { createComment, getAllComments } = require('./comment.controller');
 const createSchema = require('./comment.validator');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  const postId = req.query.postId;
-  const page = req.query.page || 1;
+router.get('/', async (req, res) => {
+  try {
+    const postId = req.query.postId;
+    const page = req.query.page || 1;
 
-  if (!postId)
-    return response.error(
-      req,
-      res,
-      'Error',
-      400,
-      'Cannot retrieve a post without an id'
-    );
+    if (!postId)
+      return response.error(
+        req,
+        res,
+        'Error',
+        400,
+        'Cannot retrieve a post without an id'
+      );
 
-  pagination(postId, page)
-    .then((data) => response.succes(req, res, 'Get Comments', 200, data))
-    .catch((err) => response.error(req, res, 'Error', 400, err));
+    const comments = await getAllComments(postId, page);
+    response.succes(req, res, 'Get Comments', 200, comments);
+  } catch (err) {
+    response.error(req, res, 'Error', 400, err)
+  }
 });
 
-router.post('/', validateToken, (req, res) => {
-  const { error } = createSchema.validate(req.body);
+router.post('/', validateToken, async (req, res) => {
+  try {
+    const { error } = createSchema.validate(req.body);
 
-  if (error) {
-    const errorMessage = getValidationErrorMessage(error);
-    return response.error(req, res, 'Error', 400, errorMessage);
+    if (error) {
+      const errorMessage = getValidationErrorMessage(error);
+      throw new Error(errorMessage);
+    }
+
+    await createComment(req.body, req.userId)
+    response.succes(req, res, 'Create Comment', 200, 'Comment Created Succesfully');
   }
-
-  createComment(req.body, req.userId)
-    .then((data) => response.succes(req, res, 'Create Comment', 200, data))
-    .catch((err) => response.error(req, res, 'Error', 400, err));
+  catch (err) {
+    response.error(req, res, 'Error', 400, err)
+  }
 });
 
 module.exports = router;
